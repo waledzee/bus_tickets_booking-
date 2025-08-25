05aee7093cd74a2a85c4a1d544f728c306732f67
waledzee <waledzaki201@gmail.com>
2025-08-25 12:22:53 +0300

    here we go

---
+++ src/main/java/com/example/bus_booking_tickets/BusBookingTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bus_booking_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bus_booking_tickets/BusBookingTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bus_booking_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusBookingTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
